532bc2737be89e67bbf75f4603049e5461550d7a
derrrick william <williamderrick100@gmail.com>
2019-03-24 07:26:07 +0300

    Edited the Models.

--- community/models.py
+++ community/models.py
@@ -7,19 +7,22 @@ class Neighbourhood(models.Model):
     neighbourhood_name = models.CharField(max_length=30)
     hood_location = models.CharField(max_length=30)
     occupants_count = models.IntegerField(default=0)
+    user = models.ForeignKey(User)
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    user = models.OneToOneField(User)
     bio = models.CharField(max_length = 100)
     profile_pic = models.ImageField(upload_to='profile/')
     neighbourhood = models.ForeignKey('Neighbourhood',blank=True,null=True)
+    contact =models.CharField(max_length=100,bank=True)
 
 class Bussiness(models.Model):
+    photo = models.ImageField(upload_to='bussiness/',default='bussiness/bdefault.jpg')
     name = models.CharField(max_length)
-    profile = models.ForeignKey('Profile',related_name='profiles')
+    user = models.ForeignKey(User)
     neighbourhood = models.ForeignKey('Neighbourhood',related_name='business')
     email = models.CharField(max_length=30)
+
+
 
 
