53ff671c7cbc9be454d0db57695251f27d99047d
derrrick william <williamderrick100@gmail.com>
2019-03-24 08:17:28 +0300

    Made migrations.

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile,Bussiness,Neighbourhood
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Bussiness)
--- community/models.py
+++ community/models.py
@@ -3,25 +3,29 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Neighbourhood(models.Model):
-    neighbourhood_name = models.CharField(max_length=30)
-    hood_location = models.CharField(max_length=30)
-    occupants_count = models.IntegerField(default=0)
-    user = models.ForeignKey(User)
 
 class Profile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     bio = models.CharField(max_length = 100)
     profile_pic = models.ImageField(upload_to='profile/')
     neighbourhood = models.ForeignKey('Neighbourhood',blank=True,null=True)
     contact =models.CharField(max_length=100,blank=True)
+    pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField(max_length=30,blank=True)
+    hood_location = models.CharField(max_length=30,blank=True)
+    occupants_count = models.IntegerField(default=0,blank=True)
+    admin = models.ForeignKey(Profile, related_name='hoods', null=True)
 
 class Bussiness(models.Model):
     photo = models.ImageField(upload_to='bussiness/',default='bussiness/bdefault.jpg')
     name = models.CharField(max_length=30)
     user = models.ForeignKey(User)
-    neighbourhood = models.ForeignKey('Neighbourhood',related_name='business')
+    neighbourhood = models.ForeignKey('Neighbourhood',related_name='businesses')
+    profile = models.ForeignKey(Profile, related_name='profiles')
     email = models.CharField(max_length=30)
+
 
 
 
