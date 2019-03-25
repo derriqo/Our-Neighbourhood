70c9c31d382b99e614dec960229a5bbae09fb521
derrrick william <williamderrick100@gmail.com>
2019-03-25 10:13:10 +0300

    Addded a method to edit profile

--- community/views.py
+++ community/views.py
@@ -12,3 +12,17 @@ def home(request):
 def profile(request):
     current__user = request.user
     return render(request,'profile/profile.html')
+
+def edit_profile(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user.id)
+    if request.method == 'POST':
+        signup_form = EditForm(request.POST, request.FILES,instance=request.user.profile) 
+        if signup_form.is_valid():
+            signup_form.save()
+            return redirect('profile')
+    else:
+        signup_form =EditForm() 
+        
+    return render(request, 'profile/update_profile.html', {"date": date, "form":signup_form,"profile":profile})
