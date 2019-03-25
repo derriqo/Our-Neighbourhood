3d0dbdb03dfb166b25b0a6897cd0c3b2b1d6602d
derrrick william <williamderrick100@gmail.com>
2019-03-25 17:50:16 +0300

    Updated models.

--- community/apps.py
+++ community/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class CommunityConfig(AppConfig):
     name = 'community'
+    def ready(self):
+        import community.signals
--- community/models.py
+++ community/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -17,6 +19,15 @@ class Profile(models.Model):
     
     def save_profile(self):
         self.save()
+
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+
+    @receiver(post_save,sender=User)
+    def save_user_profile(sender,instance, **kwargs):
+        instance.profile.save()
 
 
 class Neighbourhood(models.Model):
