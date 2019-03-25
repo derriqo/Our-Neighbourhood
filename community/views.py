ca94d68666f4d29343fd859c94d7c10b3d397522
derrrick william <williamderrick100@gmail.com>
2019-03-25 07:33:42 +0300

    Modified templates.

--- community/views.py
+++ community/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Profile,Neighbourhood,Bussiness
 
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+
+# def profile(request):
