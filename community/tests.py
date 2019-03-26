49b7e864bae03e2e3f6d7d3c2135cf31801f546b
derrrick william <williamderrick100@gmail.com>
2019-03-26 07:46:52 +0300

    Finished Profile Tests

--- community/tests.py
+++ community/tests.py
@@ -4,3 +4,23 @@ from django.contrib.auth.models import User
 
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.user=User(username='derrick',email='derrick@gmail.com',password='qwerty123')
+        self.user.save()
+
+        self.derrick= Profile(profile_pic ='derrick.jpg',bio='junior developer',neighbourhood_id=1,email='derrick@gmail.com',  user=self.user)
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.derrick, Profile))
+
+    #Testing Save Method
+    def test_save_method(self):
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+
+    def test_create_method(self):
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+
