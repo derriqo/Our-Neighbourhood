cd79ef70c87cc6b6961fffdcfa7c4e2eef650ca5
derrrick william <williamderrick100@gmail.com>
2019-03-26 07:58:13 +0300

    Tests.

--- community/tests.py
+++ community/tests.py
@@ -24,3 +24,17 @@ class ProfileTestClass(TestCase):
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles)>0)
 
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.user=User(username='derrick',email='derrick@gmail.com',password='qwerty123')
+        self.user.save()
+        self.derrick= Profile(profile_pic ='derrick.jpg',bio='junior developer',neighbourhood_id=1,email='derrick@gmail.com',  user=self.user)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Neighbourhood.objects.all().delete()
+
+    #Tests
+    def test_save_profile(self):
+        neighbourhoods = Neighbourhood.objects.all()
+        self.assertTrue(len(neighbourhoods)>0)
